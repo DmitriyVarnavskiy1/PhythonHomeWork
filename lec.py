c3417df60455f3278a280bc0522e4d49b6300af9
Dmitriy.Varnavskiy <DmitriyVarnavskiy1@yandex.ru>
2022-09-24 14:18:43 +0300

    Домашнее задание. 1 Семинар

---
+++ lec.py
@@ -0,0 +1,88 @@
+# print('hello world')
+value = None
+# 4print(type(value))
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(type(b))
+# value = 12334
+# print(type(value))
+
+# s = 'hello \nworld'
+
+# print(s) #Вывод строки
+# print(a, b, s)
+
+# print('Введите а')
+# a= float(input())
+# print('Введите b')
+# b=float(input())
+# print(a, ' + ', b, ' = ', a+b)
+# a = 1.3123312
+# b = 3
+# c=round(a * b, 5) # // деление целых чисел # возведение в степень **
+# print(c)
+
+# a = 3
+# a += 5
+
+# print(a)
+
+# f=[1,2,3,4]
+# print(f)
+# print(2 in f)
+
+# is_odd = not f[0] % 2
+# print(is_odd)
+
+#a = int(input('a= '))
+#b = int(input('b='))
+#if a>b:
+#    print(a)
+#else:
+ #   print(b)
+
+# name=input('Введите имя: ')
+# if name == 'Даша':
+  #  print('Ура, это же Даша!!!')
+# elif name=='Мама':
+   # print('Мамулечка, ура!')
+# elif name=='Ильнар':
+#    print('Ильнар топ')
+# else:
+#    print('Привет, ', name)  
+
+# original=23
+# inverted=0
+# while original !=0:
+  #  inverted=inverted*10+(original%10)
+   # original //=10
+# else:
+#    print('Пожалуй')
+ #   print('хватит')
+# print(inverted)  
+
+# for i in range(1, 10, 2):
+#    print(i)
+
+#text= 'съешь еще этих мягких французских булок'
+#print(len(text))  #39
+#print('еще' in text) #true
+#print(text.isdigit()) #false
+#print(text.islower()) #true
+#print(text.replace('еще','ЕЩЕ')) #
+
+#for c in text:
+#    print(c)
+
+def f(x):
+    if x==1:
+        return 'целое'
+    elif x== 2.3:
+        return 23
+    else:
+        return
+
+arg=2
+print(f(arg))
+print(type(f(arg)))
---
+++ task0.py
@@ -0,0 +1,6 @@
+# Вывести квадрат числа
+
+number=int(input('Введите число '))
+print('Квадрат числа {} равен {}'.format(number, number**2))
+print(f'Квадрат числа {number} равен { number**2 }')
+print('Квадрат числа', number, 'равен', number**2)
