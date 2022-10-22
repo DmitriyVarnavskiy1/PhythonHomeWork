a9cad70d4a303d1d2db532d3a0d3008034518b91
Dmitriy.Varnavskiy <DmitriyVarnavskiy1@yandex.ru>
2022-10-22 22:41:56 +0300

    HomeWork 8 version 3.0

--- task40.py
+++ task40.py
@@ -6,33 +6,43 @@
 Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой матрицы складываем с
 первым элементом первой строки второй матрицы и т.д. """
 
-
 class Matrix:
-    def __init__(self, my_list):
-        self.my_list = my_list
+    def __init__(self, list_of_lists):
+        self.mat = list_of_lists
 
     def __str__(self):
-        for row in self.my_list:
-            for i in row:
-                print(f"{i:4}", end="")
-            print()
-        return ''
-
-    '''2 варианта реализации __str__'''
-
-    # def __str__(self):
-    #     return '\n'.join(map(str, self.my_list))
-
-    # def __str__(self):
-    #     return str('\n'.join(['\t'.join([str(el) for el in i]) for i in self.my_list]))
+        string = ''
+        for i in self.mat:
+            for j in i:
+                string = string + '%s\t' % (j)
+            string = string[:-1]
+            string = string + '\n'
+        string = string[:-1]
+        return string
 
     def __add__(self, other):
-        for i in range(len(self.my_list)):
-            for i_2 in range(len(other.my_list[i])):
-                self.my_list[i][i_2] = self.my_list[i][i_2] + other.my_list[i][i_2]
-        return Matrix.__str__(self)
+        result = []
+        numbers = []
+        for i in range(len(self.mat)):
+            for j in range(len(self.mat[0])):
+                summa = other.mat[i][j] + self.mat[i][j]
+                numbers.append(summa)
+                if len(numbers) == len(self.mat[0]):
+                    result.append(numbers)
+                    numbers = []
+        return Matrix(result)
 
 
-m = Matrix([[-1, 0, 1], [-1, 0, 1], [0, 1, -1], [1, 1, -1]])
-new_m = Matrix([[-2, 0, 2], [-2, 0, 2], [0, 2, -2], [2, 2, -7]])
-print(m.__add__(new_m))
+a = [[3, 5, 32], [2, 4, 6], [-1, 64, -8]]
+b = [[2, 3, 3], [-2, 1, -6], [5, -3, 0]]
+m = Matrix(a)
+mm = Matrix(b)
+
+print("\nМатрица №1")
+print(m.__str__(), "\n")
+
+print("Матрица №2")
+print(mm.__str__(), "\n")
+
+print("Сумма матриц №1 и №2")
+print(m + mm)
